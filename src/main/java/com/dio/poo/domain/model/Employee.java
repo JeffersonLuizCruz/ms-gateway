7fa0f4172deab23feb52430258d0fe760c57fb3a
Jeff <jlcruz@stefanini.com>
2022-10-09 18:46:32 -0300

    Adiciona entidades de modelo

---
+++ src/main/java/com/dio/poo/domain/model/Employee.java
@@ -0,0 +1,10 @@
+package com.dio.poo.domain.model;
+
+import java.math.BigDecimal;
+
+public class Employee extends Person{
+
+    private String departament;
+    private Boolean isActive;
+    private BigDecimal wage;
+}
---
+++ src/main/java/com/dio/poo/domain/model/Person.java
@@ -0,0 +1,16 @@
+package com.dio.poo.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor @AllArgsConstructor
+public abstract class Person {
+
+    private String name;
+    private int age;
+    private Address address;
+    private String profession;
+
+}
